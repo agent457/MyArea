54c599ab6fa66c1d52b51ba62adc07d0d9e5e65a
agent457 <afekbn@gmail.com>
2024-09-13 23:33:46 +0300

    Rewrite

--- app/src/main/java/com/example/myarea/MainActivity.java
+++ app/src/main/java/com/example/myarea/MainActivity.java
@@ -1,263 +1,173 @@
 package com.example.myarea;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    LocationManager locationManager;
-    double longitudeBest, latitudeBest;
-    double longitudeGPS, latitudeGPS;
-    double longitudeNetwork, latitudeNetwork;
-    TextView bestLongitudeValue, bestLatitudeValue;
-    TextView gpsLongitudeValue, gpsLatitudeValue;
-    TextView netLongitudeValue, netLatitudeValue;
-    Button enableGPS, enableNETWORK, enableBEST;
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+
+    public static final int PERMISSIONS_FINE_LOCATION = 99;
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates;
+    Switch sw_locationsupdates, sw_gps;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        bestLongitudeValue = (TextView) findViewById(R.id.BestLongitudeValue);
-        bestLatitudeValue = (TextView) findViewById(R.id.BestLatitudeValue);
-        gpsLongitudeValue = (TextView) findViewById(R.id.GpsLongitudeValue);
-        gpsLatitudeValue = (TextView) findViewById(R.id.GpsLatitudeValue);
-        netLongitudeValue = (TextView) findViewById(R.id.NetLongitudeValue);
-        netLatitudeValue = (TextView) findViewById(R.id.NetLatitudeValue);
-        enableBEST = (Button) findViewById(R.id.locationControllerBEST);
-        enableGPS = (Button) findViewById(R.id.locationControllerGPS);
-        enableNETWORK = (Button) findViewById(R.id.locationControllerNETWORK);
-    }
+        tv_lat=(TextView) findViewById(R.id.tv_lat);
+        tv_lon=(TextView) findViewById(R.id.tv_lon);
+        tv_altitude=(TextView) findViewById(R.id.tv_altitude);
+        tv_accuracy=(TextView) findViewById(R.id.tv_accuracy);
+        tv_speed=(TextView) findViewById(R.id.tv_speed);
+        tv_sensor=(TextView) findViewById(R.id.tv_sensor);
+        tv_updates=(TextView) findViewById(R.id.tv_updates);
+        sw_locationsupdates =(Switch) findViewById(R.id.sw_locationsupdates);
+        sw_gps =(Switch) findViewById(R.id.sw_gps);
 
-    private boolean checkLocation() {
-        if (!isLocationEnabled())
-            showAlert();
-        return isLocationEnabled();
-    }
+        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(2000)
+                .setMaxUpdateDelayMillis(100)
+                .build();
+        locationCallBack = new LocationCallback() {
 
-    public boolean isLocationEnabled() {
-        return (locationManager.isProviderEnabled(locationManager.GPS_PROVIDER) || (locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)));
-    }
-
-    private void showAlert() {
-        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Enable Location")
-                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
-                        "use this app")
-                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(myIntent);
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                    }
-                });
-        dialog.show();
-    }
-
-    public void toggleGPSUpdates(View view) {
-        if (!checkLocation())
-            return;
-        Button button = (Button) view;
-        if (button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerGPS);
-            button.setText(R.string.resume);
-        } else {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                updateUIValues(locationResult.getLastLocation());
             }
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
-            button.setText(R.string.pause);
-        }
-    }
-
-    public void toggleBestUpdates(View view) {
-        if (!checkLocation())
-            return;
-        Button button = (Button) view;
-        if (button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerBest);
-            button.setText(R.string.resume);
-        } else {
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            criteria.setAltitudeRequired(false);
-            criteria.setBearingRequired(false);
-            criteria.setCostAllowed(true);
-            criteria.setPowerRequirement(Criteria.POWER_LOW);
-            String provider = locationManager.getBestProvider(criteria, true);
-            if (provider != null) {
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+        };
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_gps.isChecked()){
+                    locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
+                            .setWaitForAccurateLocation(false)
+                            .setMinUpdateIntervalMillis(2000)
+                            .setMaxUpdateDelayMillis(100)
+                            .build();
+                    tv_sensor.setText("Using GPS sensors");
                 }
-                locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
-                button.setText(R.string.pause);
-                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+                else{
+                    locationRequest = new LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY, 100)
+                            .setWaitForAccurateLocation(false)
+                            .setMinUpdateIntervalMillis(2000)
+                            .setMaxUpdateDelayMillis(100)
+                            .build();
+                    tv_sensor.setText("Using Towers + WIFI");
+                }
             }
-        }
-    }
+        });
 
-    public void toggleNetworkUpdates(View view) {
-        if (!checkLocation())
-            return;
-        Button button = (Button) view;
-        if (button.getText().equals(getResources().getString(R.string.pause))) {
-            locationManager.removeUpdates(locationListenerNetwork);
-            button.setText(R.string.resume);
-        } else {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+        sw_locationsupdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_locationsupdates.isChecked()){
+                    StartLocationUpdates();
+                }
+                else{
+                    stopLocationUpdates();
+                }
             }
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
-            Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
-            button.setText(R.string.pause);
-        }
+        });
+
+        updateGPS();
     }
-    private final LocationListener locationListenerBest = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitudeBest = location.getLongitude();
-            latitudeBest = location.getLatitude();
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    bestLongitudeValue.setText(longitudeBest + "");
-                    bestLatitudeValue.setText(latitudeBest + "");
-                    Toast.makeText(MainActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
+    private void stopLocationUpdates() {
+        tv_updates.setText("Location is not being tracked");
+        tv_lat.setText("Not tracking location");
+        tv_lon.setText("Not tracking location");
+        tv_speed.setText("Not tracking location");
+        tv_accuracy.setText("Not tracking location");
+        tv_altitude.setText("Not tracking location");
+        tv_sensor.setText("Not tracking location");
 
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
 
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-    private final LocationListener locationListenerNetwork = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitudeNetwork = location.getLongitude();
-            latitudeNetwork = location.getLatitude();
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    netLongitudeValue.setText(longitudeNetwork + "");
-                    netLatitudeValue.setText(latitudeNetwork + "");
-                    Toast.makeText(MainActivity.this, "Network Provider update", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-    private final LocationListener locationListenerGPS = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitudeGPS = location.getLongitude();
-            latitudeGPS = location.getLatitude();
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    gpsLongitudeValue.setText(longitudeGPS + "");
-                    gpsLatitudeValue.setText(latitudeGPS + "");
-                    Toast.makeText(MainActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-
+    @SuppressLint("MissingPermission")
+    private void StartLocationUpdates() {
+        tv_updates.setText("Location is being tracked");
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
 
     @Override
-    public void onClick(View v) {
-        if (v == enableGPS){
-            toggleGPSUpdates(v);
-        }
-        if (v==enableNETWORK){
-            toggleNetworkUpdates(v);
-        }
-        if (v==enableBEST){
-            toggleBestUpdates(v);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                }
+                else{
+                    Toast.makeText(this, "This app requires permission to be granted in order to work properly", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
         }
     }
+
+    private void updateGPS(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    updateUIValues(location);
+                }
+            });
+        }
+        else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+    private void updateUIValues(Location location){
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+
+        if(location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else {
+            tv_altitude.setText("Not available");
+        }
+        if(location.hasSpeed()){
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }
+        else {
+            tv_speed.setText("Not available");
+        }
+
+    }
+
 }
